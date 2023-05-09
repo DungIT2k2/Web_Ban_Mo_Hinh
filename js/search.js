2d8fdc15c24d85ada4fba80f2922cad07444c63d
Nguyễn Văn Tiến Dũng <116363146+DungIT2k2@users.noreply.github.com>
2023-05-09 23:31:10 +0700

    update

--- js/search.js
+++ js/search.js
@@ -1,12 +1,7 @@
-var searchText = document.getElementById("searchText").value;
+const searchText = document.getElementById("searchText");
+const btn_search = document.querySelector(".header__search__btn");
+const header__search = document.querySelector('.header__search');
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
-        // Handle the response from the PHP script
-        var response = xhttp.responseText;
-        console.log(response);
-    }
-};
-xhttp.open("GET", "search.php?chon=" + searchText, true);
-xhttp.send();
+btn_search.onclick = function() {
+    header__search.classList.toggle("show");
+}
