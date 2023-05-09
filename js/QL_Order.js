b15ceb05a4fdaab85d1975568e4c4aa6ef7132ac
Nguyen Chi Cong <chicongkhpro@gmail.com>
2023-05-10 00:31:40 +0700

    update_code

--- js/QL_Order.js
+++ js/QL_Order.js
@@ -1,102 +1,103 @@
 const cthoadon_overlay = document.querySelector(".overplay_OrderDetail");
 const cthoadon_close = document.querySelector(".btn_X");
-const cthoadon_overlay_behind = document.querySelector(".overplay__behind_OrderDetail");
+const cthoadon_overlay_behind = document.querySelector(
+  ".overplay__behind_OrderDetail"
+);
 const content_orderdetail = document.querySelector(".content_OrderDetail");
 function XemChiTiet(iddonhang) {
-    taoCTDH(iddonhang);
-    cthoadon_overlay.classList.toggle("show");
-
+  taoCTDH(iddonhang);
+  cthoadon_overlay.classList.toggle("show");
 }
 cthoadon_close.addEventListener("click", function () {
-    cthoadon_overlay.classList.toggle("show");
+  cthoadon_overlay.classList.toggle("show");
 });
 cthoadon_overlay_behind.addEventListener("click", function () {
-    cthoadon_overlay.classList.toggle("show");
+  cthoadon_overlay.classList.toggle("show");
 });
 
 function taoCTDH(iddonhang) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "./DAL/DAL_DetailOrder.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            // Xử lý kết quả trả về từ PHP
-            var response = xhr.responseText;
-            content_orderdetail.innerHTML = response;
-            getCTDH(iddonhang);
-        }
-    };
-    xhr.send("iddh=" + iddonhang + "&phuongthuc=tao");
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "./DAL/DAL_DetailOrder.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      // Xử lý kết quả trả về từ PHP
+      var response = xhr.responseText;
+      content_orderdetail.innerHTML = response;
+      getCTDH(iddonhang);
+    }
+  };
+  xhr.send("iddh=" + iddonhang + "&phuongthuc=tao");
 }
 function getCTDH(iddonhang) {
-    const tbody_CTDH = document.getElementById("tbody_CTDH");
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "./DAL/DAL_DetailOrder.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            // Xử lý kết quả trả về từ PHP
-            var response = xhr.responseText;
-            tbody_CTDH.innerHTML = response;
-        }
-    };
-    xhr.send("iddh=" + iddonhang + "&phuongthuc=get");
+  const tbody_CTDH = document.getElementById("tbody_CTDH");
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "./DAL/DAL_DetailOrder.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      // Xử lý kết quả trả về từ PHP
+      var response = xhr.responseText;
+      tbody_CTDH.innerHTML = response;
+    }
+  };
+  xhr.send("iddh=" + iddonhang + "&phuongthuc=get");
 }
 function checkedUser(iddonhang) {
-    var checkbox = document.getElementsByName("confirm_user");
-    for (var i = 0; i < checkbox.length; i++) {
-        if (checkbox[i].checked) {
-            checkbox[i].disabled = true;
-        }
+  var checkbox = document.getElementsByName("confirm_user");
+  for (var i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked) {
+      checkbox[i].disabled = true;
     }
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "./DAL/DAL_DetailOrder.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            // Xử lý kết quả trả về từ PHP
-            var response = xhr.responseText;
-            console.warn(response);
-            if (response == 1) {
-                alert("Xác nhận đơn hàng thành công!");
-            }
-        }
-    };
-    xhr.send("iddh=" + iddonhang + "&phuongthuc=checkeduser");
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "./DAL/DAL_DetailOrder.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      // Xử lý kết quả trả về từ PHP
+      var response = xhr.responseText;
+      console.warn(response);
+      if (response == 1) {
+        alert("Xác nhận đơn hàng thành công!");
+      }
+    }
+  };
+  xhr.send("iddh=" + iddonhang + "&phuongthuc=checkeduser");
 }
 function Confirm_DH() {
-    const iddonhang = document.getElementById("MaDH");
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "./DAL/DAL_DetailOrder.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            // Xử lý kết quả trả về từ PHP
-            var response = xhr.responseText;
-            if (response == 1) {
-                alert("Xác nhận đơn hàng thành công!");
-                taoCTDH(iddonhang.textContent);
-                getCTDH(iddonhang.textContent);
-                reload_QLDH();
-            }
-        }
+  const iddonhang = document.getElementById("MaDH");
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "./DAL/DAL_DetailOrder.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      // Xử lý kết quả trả về từ PHP
+      var response = xhr.responseText;
+      if (response == 1) {
+        alert("Xác nhận đơn hàng thành công!");
+        taoCTDH(iddonhang.textContent);
+        getCTDH(iddonhang.textContent);
+        reload_QLDH();
+      }
     }
-    xhr.send("iddh=" + iddonhang.textContent + "&phuongthuc=confirmuser");
+  };
+  xhr.send("iddh=" + iddonhang.textContent + "&phuongthuc=confirmuser");
 }
 const tbody_QL_Order = document.querySelector(".tbody_QLOrder");
 function reload_QLDH() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "./DAL/DAL_QL_Order.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            // Xử lý kết quả trả về từ PHP
-            var response = xhr.responseText;
-            console.log(response);
-            tbody_QL_Order.innerHTML = response;
-        }
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "./DAL/DAL_QL_Order.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      // Xử lý kết quả trả về từ PHP
+      var response = xhr.responseText;
+      console.log(response);
+      tbody_QL_Order.innerHTML = response;
     }
-    xhr.send();
+  };
+  xhr.send();
 }
 
 // Thực hiện submit Order
@@ -110,15 +111,25 @@ function submitForm() {
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       // Xử lý kết quả trả về từ PHP
-      var response = xhr.responseText;
-      tbody_QL_Order.innerHTML = response;
+      var dateStart = new Date(from_date);
+      var dateEnd = new Date(to_date);
+
+      if (dateEnd < dateStart) {
+        alert("Ngày kết thúc phải lớn hơn ngày bắt đầu");
+      } else {
+        var response = xhr.responseText;
+        tbody_QL_Order.innerHTML = response;
+      }
     }
   };
 
-  var data = "from_date=" + encodeURIComponent(from_date) + "&to_date=" + encodeURIComponent(to_date);
+  var data =
+    "from_date=" +
+    encodeURIComponent(from_date) +
+    "&to_date=" +
+    encodeURIComponent(to_date);
   xhr.send(data);
 }
-
 
 window.onload = function () {
   var from_date = document.getElementById("from-date").value;
@@ -129,11 +140,22 @@ window.onload = function () {
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       // Xử lý kết quả trả về từ PHP
-      var response = xhr.responseText;
-      tbody_QL_Order.innerHTML = response;
+      var dateStart = new Date(from_date);
+      var dateEnd = new Date(to_date);
+
+      if (dateEnd < dateStart) {
+        alert("Ngày kết thúc phải lớn hơn ngày bắt đầu");
+      } else {
+        var response = xhr.responseText;
+        tbody_QL_Order.innerHTML = response;
+      }
     }
   };
 
-  var data = "from_date=" + encodeURIComponent(from_date) + "&to_date=" + encodeURIComponent(to_date);
+  var data =
+    "from_date=" +
+    encodeURIComponent(from_date) +
+    "&to_date=" +
+    encodeURIComponent(to_date);
   xhr.send(data);
-}
+};
--- js/thongke.js
+++ js/thongke.js
@@ -4,17 +4,34 @@ function submitForm() {
   var to_date = document.getElementById("to-date").value;
   var product_type = document.getElementById("product-type").value;
   var xhr = new XMLHttpRequest();
+
+  console.log(from_date, to_date);
+
   xhr.open("POST", "./DAL/DAL_thongke.php", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       // Xử lý kết quả trả về từ PHP
-      var response = xhr.responseText;
-      document.getElementById("result").innerHTML = response;
+      var dateStart = new Date(from_date);
+      var dateEnd = new Date(to_date);
+
+      if (dateEnd < dateStart) {
+        alert("Ngày kết thúc phải lớn hơn ngày bắt đầu");
+      } else {
+        console.log("Ngày kết thúc hợp lệ");
+        var response = xhr.responseText;
+        document.getElementById("result").innerHTML = response;
+      }
     }
   };
 
-  var data = "from_date=" + encodeURIComponent(from_date) + "&to_date=" + encodeURIComponent(to_date) + "&product_type=" + encodeURIComponent(product_type);
+  var data =
+    "from_date=" +
+    encodeURIComponent(from_date) +
+    "&to_date=" +
+    encodeURIComponent(to_date) +
+    "&product_type=" +
+    encodeURIComponent(product_type);
   xhr.send(data);
 }
 
@@ -22,17 +39,34 @@ window.onload = function () {
   var from_date = document.getElementById("from-date").value;
   var to_date = document.getElementById("to-date").value;
   var product_type = document.getElementById("product-type").value;
+
+  console.log(from_date, to_date);
+
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "./DAL/DAL_thongke.php", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       // Xử lý kết quả trả về từ PHP
-      var response = xhr.responseText;
-      document.getElementById("result").innerHTML = response;
+      var dateStart = new Date(from_date);
+      var dateEnd = new Date(to_date);
+
+      if (dateEnd < dateStart) {
+        alert("Ngày kết thúc phải lớn hơn ngày bắt đầu");
+      } else {
+        console.log("Ngày kết thúc hợp lệ");
+        var response = xhr.responseText;
+        document.getElementById("result").innerHTML = response;
+      }
     }
   };
 
-  var data = "from_date=" + encodeURIComponent(from_date) + "&to_date=" + encodeURIComponent(to_date) + "&product_type=" + encodeURIComponent(product_type);
+  var data =
+    "from_date=" +
+    encodeURIComponent(from_date) +
+    "&to_date=" +
+    encodeURIComponent(to_date) +
+    "&product_type=" +
+    encodeURIComponent(product_type);
   xhr.send(data);
-}
+};
