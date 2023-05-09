3ca0846ab234b78126581e1ac5d48838eaa6d449
Nguyễn Văn Tiến Dũng <116363146+DungIT2k2@users.noreply.github.com>
2023-05-09 21:20:29 +0700

    update_ptrang

--- DAL/DAL_PhanTrang.php
+++ DAL/DAL_PhanTrang.php
@@ -4,9 +4,9 @@
     $sql = 'SELECT * FROM product sp JOIN caterogyproduct lsp ON sp.IDCaterogyProduct = lsp.IDCaterogyProduct WHERE NameCaterogyProduct="'.$tenloaisp.'"';
     $result = $conn->query($sql);
     $soluongsp = $result->num_rows;
-    $sotrang = ceil($soluongsp/3);
+    $sotrang = ceil($soluongsp/4);
     echo '<ul class="pagenumber">';
-    if ($soluongsp > 3){
+    if ($sotrang > 3){
         echo '<li class="pagenumber_item" onclick="backRenderPageNumber()">';
         echo '<a class="pagenumber_item_link fa fa-angle-left"></a></li>';
     }
@@ -14,7 +14,7 @@
         echo '<li class="pagenumber_item" onclick="handlePageNumber(\''.$tenloaisp.'\','.$i.')">';
         echo '<a class="pagenumber_item_link">'.$i.'</a></li>';
     }
-    if ($soluongsp > 3){
+    if ($sotrang > 3){
         echo '<li class="pagenumber_item" onclick="nextRenderPageNumber()">';
         echo '<a class="pagenumber_item_link fa fa-angle-right" id="angle_right"></a></li>';
     }
